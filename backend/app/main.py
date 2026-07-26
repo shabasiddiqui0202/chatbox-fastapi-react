2af557e127a63d09c06a9a609eeede75e408c74c
Shaba Siddiqui <shabasiddiqui0202@gmail.com>
2026-07-26 15:14:52 +0530

    now working

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,7 @@ except Exception:
     pass
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
